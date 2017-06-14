8e678864c556280cd7e6dfc8f6fa0c180c110909
kimizuka-fumitaka <f.kimizuka@gmail.com>
2017-06-14 15:49:56 +0900

    マイクテスト

---
+++ mic-web/js/index.js
@@ -0,0 +1,51 @@
+(function(win, doc) {
+
+  "use strict";
+  
+  let canvas = doc.getElementById("canvas"),
+      ctx    = canvas.getContext("2d");
+  
+  navigator.webkitGetUserMedia({
+    audio: true
+  }, _handleSuccess, _handleError);
+  
+  function _handleSuccess(evt) {
+    debugger;
+    let audioCtx = new AudioContext(),
+        src      = audioCtx.createMediaStreamSource(evt),
+        analyser = audioCtx.createAnalyser(evt);
+
+    let LENGTH = 256,
+        data   = new Uint8Array(LENGTH),
+        w      = 0,
+        i      = 0;
+
+
+    analyser.fftSize = 1024;
+
+    src.connect(audioCtx.destination);
+    src.connect(analyser);
+
+    setInterval(function() {
+      canvas.width  = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      ctx.fillStyle = "#3e3e3e";
+
+      w = canvas.width / LENGTH,
+
+      analyser.getByteFrequencyData(data);
+
+      for (i = 0; i < LENGTH; ++i) {
+        ctx.rect(i * w, canvas.height - data[i], w, data[i]);
+      }
+
+      ctx.fill();
+    }, 20);
+  }
+
+  function _handleError() {
+    alert("Error!");
+  }
+
+})(this, document);
